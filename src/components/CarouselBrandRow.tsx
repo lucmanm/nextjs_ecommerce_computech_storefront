5e7ecf17c0f0023a1f87385076cc78c21f9378e5
lucmanm <greenjamez22@gmail.com>
2024-05-18 01:51:34 +0300

    Link applied on in the image slider

--- src/components/CarouselBrandRow.tsx
+++ src/components/CarouselBrandRow.tsx
@@ -11,6 +11,7 @@ import useTextDirection from "@/hook/useTextDirection";
 import { getBrands } from "@/actions/getBrand";
 import { useLocale } from "next-intl";
 import Image from "next/image";
+import Link from "next/link";
 
 // TypeProps
 type TBrand = {
@@ -33,15 +34,18 @@ export async function CarouselBrandRow() {
     >
       <CarouselContent>
         {brandData.map((data, index) => (
+          
           <CarouselItem
             key={index}
-            className="basis-1/4 rounded md:basis-1/6 lg:basis-1/12"
+            className="basis-1/4  md:basis-1/6 lg:basis-1/12"
           >
             <div className="p-1 text-center text-xs lg:text-base">
               <Card>
+                <Link href={`/${locale}/product/${data.name}`}>
                 <CardContent className="flex aspect-square items-center justify-center p-6">
                   <Image alt={data.name} src={data.imageUrl} width={400} height={400}/>
                 </CardContent>
+                </Link>
               </Card>
               <span className="font-semibold capitalize">{data.name}</span>
             </div>
